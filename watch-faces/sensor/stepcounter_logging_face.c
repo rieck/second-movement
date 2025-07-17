d76c785d130545e6390e7def74300bb2da2e1889
Konrad Rieck <konrad@mlsec.org>
2025-07-17 10:34:53 +0200

    Implementation of a simple face to log raw accelerometer data

---
+++ watch-faces/sensor/stepcounter_logging_face.c
@@ -0,0 +1,268 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Konrad Rieck
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "stepcounter_logging_face.h"
+#include "watch.h"
+#include "watch_utility.h"
+#include "filesystem.h"
+#include "lfs.h"
+
+/* Access to file system */
+extern lfs_t eeprom_filesystem;
+
+/* Constants*/
+#define LOG_FILE_NAME       "sc_log.bin"
+#define LOG_FILE_SPACER     0xff
+#define ERROR_OPEN_FILE     0x01
+#define ERROR_CLOSE_FILE    0x02
+#define ERROR_WRITE_HEADER  0x03
+#define ERROR_WRITE_SPACER  0x04
+#define ERROR_WRITE_DATA    0x05
+#define MAX_DURATION        100
+
+/* Storage format */
+#define STORE_SQ_MAGNITUDE
+/* #define STORE_16_READINGS */
+
+static void _start_recording(stepcounter_logging_state_t *state)
+{
+    uint32_t ret = 0;
+    printf("Starting recording (index: %d)\n", state->index);
+
+    /* Open log file */
+    int err = lfs_file_open(&eeprom_filesystem, &state->file, LOG_FILE_NAME, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_APPEND);
+    if (err < 0) {
+        state->error = ERROR_OPEN_FILE;
+        return;
+    }
+
+    /* Initialize log index and start time */
+    watch_date_time_t now = watch_rtc_get_date_time();
+    uint32_t now_ts = watch_utility_date_time_to_unix_time(now, 0);
+    state->start_ts = now_ts;
+
+    /* Write log header */
+    ret += lfs_file_write(&eeprom_filesystem, &state->file, &state->index, sizeof(state->index));
+    ret += lfs_file_write(&eeprom_filesystem, &state->file, &state->start_ts, sizeof(state->start_ts));
+    if (ret != sizeof(state->index) + sizeof(state->start_ts)) {
+        state->error = ERROR_WRITE_HEADER;
+        return;
+    }
+}
+
+static void _stop_recording(stepcounter_logging_state_t *state)
+{
+    uint32_t ret = 0;
+    int8_t spacer = LOG_FILE_SPACER;
+    printf("Stopping recording (index: %d)\n", state->index);
+
+    /* Write spacer */
+    ret += lfs_file_write(&eeprom_filesystem, &state->file, &spacer, sizeof(spacer));
+    if (ret != sizeof(spacer)) {
+        state->error = ERROR_WRITE_SPACER;
+        return;
+    }
+
+    lfs_file_sync(&eeprom_filesystem, &state->file);
+    printf("Synced log file\n");
+    uint32_t size = lfs_file_tell(&eeprom_filesystem, &state->file);
+    printf("File size: %lu\n", size);
+
+    /* Close log file */
+    int err = lfs_file_close(&eeprom_filesystem, &state->file);
+    if (err < 0) {
+        state->error = ERROR_CLOSE_FILE;
+        return;
+    }
+
+    /* Update log index and reset time */
+    state->index++;
+    state->start_ts = 0;
+}
+
+static void _log_data(stepcounter_logging_state_t *state, lis2dw_fifo_t *fifo)
+{
+    uint32_t ret = 0;
+    printf("Logging data (%d measurements)\n", fifo->count);
+
+    /* Store fifo count (8 bit) */
+    ret = lfs_file_write(&eeprom_filesystem, &state->file, &fifo->count, sizeof(fifo->count));
+    if (ret != sizeof(fifo->count))
+        goto error;
+
+    for (uint8_t cnt = 0; cnt < fifo->count; cnt++) {
+#ifdef STORE_16_READINGS
+        /* Write readings data (48bit) */
+        ret = 0;
+        ret += lfs_file_write(&eeprom_filesystem, &state->file, &fifo->readings[cnt].x, sizeof(fifo->readings[cnt].x));
+        ret += lfs_file_write(&eeprom_filesystem, &state->file, &fifo->readings[cnt].y, sizeof(fifo->readings[cnt].y));
+        ret += lfs_file_write(&eeprom_filesystem, &state->file, &fifo->readings[cnt].z, sizeof(fifo->readings[cnt].z));
+        if (ret != 3 * sizeof(fifo->readings[cnt].x))
+            goto error;
+#endif
+
+#ifdef STORE_SQ_MAGNITUDE
+        /* Write squared magnitude of readings (32bit) */
+        float mag = fifo->readings[cnt].x * fifo->readings[cnt].x +
+                    fifo->readings[cnt].y * fifo->readings[cnt].y +
+                    fifo->readings[cnt].z * fifo->readings[cnt].z;
+        ret = lfs_file_write(&eeprom_filesystem, &state->file, &mag, sizeof(mag));
+        if (ret != sizeof(mag))
+            goto error;
+#endif
+    }
+    return;
+
+  error:
+    state->error = ERROR_WRITE_DATA;
+    return;
+}
+
+static void _display_state(stepcounter_logging_state_t *state)
+{
+    char buf[10];
+
+    watch_clear_colon();
+    snprintf(buf, sizeof(buf), "%d", state->index);
+    watch_display_text_with_fallback(WATCH_POSITION_TOP_RIGHT, buf, buf);
+    watch_display_text_with_fallback(WATCH_POSITION_TOP_LEFT, "SL", "SL");
+
+    if (state->error) {
+        snprintf(buf, sizeof(buf), "E %.2d  ", state->error);
+        watch_display_text_with_fallback(WATCH_POSITION_BOTTOM, buf, buf);
+        return;
+    }
+
+    if (!state->start_ts) {
+        int32_t free_space = filesystem_get_free_space();
+        snprintf(buf, sizeof(buf), "F %.4ld", free_space);
+        watch_display_text_with_fallback(WATCH_POSITION_BOTTOM, buf, buf);
+        return;
+    }
+
+    watch_date_time_t now = watch_rtc_get_date_time();
+    uint32_t now_ts = watch_utility_date_time_to_unix_time(now, 0);
+    uint32_t diff = now_ts - state->start_ts;
+    snprintf(buf, sizeof(buf), "R %.4lu", diff);
+    watch_display_text_with_fallback(WATCH_POSITION_BOTTOM, buf, buf);
+}
+
+void stepcounter_logging_face_setup(uint8_t watch_face_index, void **context_ptr)
+{
+    (void) watch_face_index;
+    if (*context_ptr == NULL) {
+        *context_ptr = malloc(sizeof(stepcounter_logging_state_t));
+        memset(*context_ptr, 0, sizeof(stepcounter_logging_state_t));
+
+        /* 
+         * Configure accelerometer to run in background at 12.5 Hz sampling rate.
+         * This rate is required for step counting since human walking/running 
+         * movements occur at frequencies up to 5 Hz
+         */
+        movement_set_accelerometer_background_rate(LIS2DW_DATA_RATE_12_5_HZ);
+
+        /*
+         * Enable lis2dw FIFO to collect data. The FIFO can hold up to 32 samples 
+         * of measurements, enabling sampling rates of 12.5 Hz and 25 Hz when 
+         * processing data every second.
+         */
+        lis2dw_enable_fifo();
+
+        /* 
+         * Moreover, we assume the accelerometer is configured by default as follows 
+         * (see lis2dw_begin in lis2dw.c):
+         *  - Low power mode enabled
+         *  - LP mode 1 (12-bit)
+         *  - Bandwidth filtering ODR/2 (6.25 Hz)
+         *  - ±2g range
+         */
+    }
+
+    stepcounter_logging_state_t *state = (stepcounter_logging_state_t *) * context_ptr;
+    state->index = 1;
+}
+
+void stepcounter_logging_face_activate(void *context)
+{
+    stepcounter_logging_state_t *state = (stepcounter_logging_state_t *) context;
+    _display_state(state);
+}
+
+bool stepcounter_logging_face_loop(movement_event_t event, void *context)
+{
+    stepcounter_logging_state_t *state = (stepcounter_logging_state_t *) context;
+    lis2dw_fifo_t fifo;
+
+    switch (event.event_type) {
+        case EVENT_ACTIVATE:
+            _display_state(state);
+            break;
+        case EVENT_TICK:
+            if (state->start_ts) {
+                lis2dw_read_fifo(&fifo);
+                _log_data(state, &fifo);
+                lis2dw_clear_fifo();
+
+                watch_date_time_t now = watch_rtc_get_date_time();
+                uint32_t now_ts = watch_utility_date_time_to_unix_time(now, 0);
+                uint32_t diff = now_ts - state->start_ts;
+
+                /* Check if recording duration exceeds maximum */
+                if (diff > MAX_DURATION) {
+                    _stop_recording(state);
+                }
+            }
+
+            _display_state(state);
+            break;
+        case EVENT_ALARM_BUTTON_DOWN:
+            if (!state->start_ts)
+                _start_recording(state);
+            else
+                _stop_recording(state);
+            _display_state(state);
+            break;
+        default:
+            movement_default_loop_handler(event);
+            break;
+    }
+
+    return true;
+}
+
+void stepcounter_logging_face_resign(void *context)
+{
+    stepcounter_logging_state_t *state = (stepcounter_logging_state_t *) context;
+    if (state->start_ts)
+        _stop_recording(state);
+}
+
+movement_watch_face_advisory_t stepcounter_logging_face_advise(void *context)
+{
+    (void) context;
+    movement_watch_face_advisory_t retval = { 0 };
+    return retval;
+}
---
+++ watch-faces/sensor/stepcounter_logging_face.h
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Konrad Rieck
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Experimental watch face for recording accelerometer data, intended to support
+ * step counter development. The face can record x, y, z readings or the squared
+ * magnitude of the readings.
+ *
+ * Behavior:
+ * - On startup, the face displays the available filesystem space, labeled with "F".
+ * - The number in the top right indicates the current recording session number.
+ * - Pressing the alarm button starts recording. While recording:
+ *     - The display shows "r" to indicate active recording.
+ *     - The elapsed recording time in seconds is shown.
+ * - Pressing the alarm button again stops recording.
+ * - After stopping, the session number is incremented, and the available space is shown.
+ * - If an error occurs, the display shows "E" followed by the error code.
+ */
+
+#include "movement.h"
+#include "lfs.h"
+
+typedef struct {
+    lfs_file_t file;
+    uint32_t start_ts;
+    uint8_t index;
+    uint8_t error;
+} stepcounter_logging_state_t;
+
+void stepcounter_logging_face_setup(uint8_t watch_face_index, void **context_ptr);
+void stepcounter_logging_face_activate(void *context);
+bool stepcounter_logging_face_loop(movement_event_t event, void *context);
+void stepcounter_logging_face_resign(void *context);
+movement_watch_face_advisory_t stepcounter_logging_face_advise(void *context);
+
+#define stepcounter_logging_face ((const watch_face_t){ \
+    stepcounter_logging_face_setup, \
+    stepcounter_logging_face_activate, \
+    stepcounter_logging_face_loop, \
+    stepcounter_logging_face_resign, \
+    stepcounter_logging_face_advise, \
+})
